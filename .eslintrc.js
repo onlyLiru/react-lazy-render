a359711fb405c187c26af4c87f3c18aae2f850fd
wb-lr903313 <wb-lr903313@alibaba-inc.com>
2021-01-12 18:43:58 +0800

    完善lint脚本配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,31 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "es6": true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint-config-ali',
+    'eslint-config-ali/react',
+    'eslint-config-ali/jsx-a11y',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint-config-ali",
-        "eslint-config-ali/react",
-        "eslint-config-ali/jsx-a11y",
-        "plugin:prettier/recommended",
-        "prettier/react"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "allowImportExportEverywhere": false,
-        "codeFrame": true
+    ecmaVersion: 6,
+    sourceType: 'module',
+    allowImportExportEverywhere: false,
+    codeFrame: true,
+  },
+  plugins: ['react'],
+  settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
     },
-    "plugins": [
-        "react"
-    ],
-    "settings": {
-        "react": {
-            "pragma": "React",
-            "version": "detect"
-        },
-    },
-    "rules": {
-    }
+  },
+  rules: {},
 };
